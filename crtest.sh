641eba4af1611e097d65e1fbd38e500b830f5099
Tzbfire <962831443@qq.com>
2025-06-08 14:52:02 +0800

    Update crtest.sh

--- crtest.sh
+++ crtest.sh
@@ -14,7 +14,7 @@ CRON_S5="nohup ${FILE_PATH}/s5 -c ${FILE_PATH}/config.json >/dev/null 2>&1 &"
 CRON_NEZHA="nohup ${WORKDIR}/start.sh >/dev/null 2>&1 &"
 CRON_HYSTERIA="nohup ${HYSTERIA_WORKDIR}/web server -c ${HYSTERIA_CONFIG} >/dev/null 2>&1 &"
 PM2_PATH="${USER_HOME}/.npm-global/lib/node_modules/pm2/bin/pm2"
-CRON_JOB="*/12 * * * * $PM2_PATH resurrect >> ${USER_HOME}/pm2_resurrect.log 2>&1"
+CRON_JOB="1 * * * * $PM2_PATH resurrect >> ${USER_HOME}/pm2_resurrect.log 2>&1"
 
 # 定义函数来添加 crontab 任务，减少重复代码
 add_cron_job() {
@@ -35,21 +35,21 @@ else
   if [ -f "${WORKDIR}/start.sh" ]; then
     echo "添加 Nezha 的 crontab 重启任务"
     add_cron_job "@reboot pkill -kill -u $USER && ${CRON_NEZHA}"
-    add_cron_job "*/12 * * * * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}"
+    add_cron_job "1 * * * * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}"
   fi
 
   # Socks5 的重启任务
   if [ -f "${FILE_PATH}/config.json" ]; then
     echo "添加 Socks5 的 crontab 重启任务"
     add_cron_job "@reboot pkill -kill -u $USER && ${CRON_S5}"
-    add_cron_job "*/12 * * * * pgrep -x \"s5\" > /dev/null || ${CRON_S5}"
+    add_cron_job "1 * * * * pgrep -x \"s5\" > /dev/null || ${CRON_S5}"
   fi
 
   # Hysteria 的重启任务
   if [ -f "$HYSTERIA_CONFIG" ]; then
     echo "添加 Hysteria 的 crontab 重启任务"
     add_cron_job "@reboot pkill -kill -u $USER && ${CRON_HYSTERIA}"
-    add_cron_job "*/12 * * * * pgrep -x \"web\" > /dev/null || ${CRON_HYSTERIA}"
+    add_cron_job "1 * * * * pgrep -x \"web\" > /dev/null || ${CRON_HYSTERIA}"
   fi
 fi
 
